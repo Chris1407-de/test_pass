3031a7ee16acb44260232c4559b5281aaa29afa8
Chris1407-de <64892483+Chris1407-de@users.noreply.github.com>
2020-07-03 11:33:55 +0200

    Update test_branch.js

--- test/test_branch.js
+++ test/test_branch.js
@@ -2,17 +2,15 @@ let convert = require('../Test_branchCoverage.js');
 let assert = require('assert');
 
 describe('BranchCoverage', function() {
-    describe('Teststep', function() {
     it('a=0 UND b=-5', function() {
       assert.equal(true, convert.test(0, -5));
     });
-});/*
     it('a=5 UND b=5', function() {
       assert.equal(true, convert.test(5, 5));
     });
     it('a=-5 UND b=-5', function() {
       assert.equal(true, convert.test(-5, -5));
-    });*/
+    });
     /*it('a=5 UND b=-5', function() {
       assert.equal(false, convert.test(5, -5));
     });
