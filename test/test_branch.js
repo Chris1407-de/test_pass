ed5bb9730d08a9e796b7b4ea91663eb2c62cf09b
Chris1407-de <64892483+Chris1407-de@users.noreply.github.com>
2020-07-09 08:01:41 +0200

    Update test_branch.js

--- test/test_branch.js
+++ test/test_branch.js
@@ -11,13 +11,4 @@ describe('BranchCoverage', function() {
     it('a=-5 UND b=-5', function() {
       assert.equal(true, convert.test(-5, -5));
     });
-    /*it('a=5 UND b=-5', function() {
-      assert.equal(false, convert.test(5, -5));
-    });
-    it('a=5 UND b=0', function() {
-      assert.equal(false, convert.test(5, 0));
-    });
-    it('a=0 UND b=5', function() {
-      assert.equal(false, convert.test(5, -5));
-    });*/
 });
